c5e8069202e7a1751bcd7b7a7731d8783cd15806
sancho <sancho@windeath.ru>
2018-09-25 09:44:37 +0500

    изменил версию

--- dozor.stats.user.js
+++ dozor.stats.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Dozor stats
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  try to take over the world!
 // @author       sancho
 // @downloadURL  https://raw.githubusercontent.com/sancho2934489/dozor-stats/master/dozor.stats.user.js
